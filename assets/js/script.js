159ce40544d683bb25c1534680be9443be891537
JMP <jmproctor14@live.com>
2021-12-11 21:09:32 -0600

    added notes to .js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var hourColor = function() {
 
         var actualTime = moment().hours();
-
+        //changes the timeBlock color based on whether it is past, present, or future
         $(".time-block").each(function() {
             var hourBlock = parseInt($(this).attr("id".split("-")[1]))
             
@@ -40,7 +40,7 @@ $(document).ready(function() {
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
-    
+
     //displays current day & time
     $("#currentDay").text(moment().format("LLLL"));
 })
